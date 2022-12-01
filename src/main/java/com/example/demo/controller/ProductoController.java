5524f6a78616792f9409c9e2300ae37211a0c53a
breyna <breyna@cientifica.edu.pe>
2022-11-30 20:32:46 -0500

    v1.5

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -3,7 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,28 @@ public class ProductoController {
 	@GetMapping("/all")
 	public List<Producto> listar(){
 		return productoService.readAll();
+	}
+	
+	@PostMapping("/add")
+	public Producto add(@RequestBody Producto prod){
+		return productoService.create(prod);
+	}
+	@GetMapping("/{id}")
+	public Producto read(@PathVariable int id){
+		return productoService.read(id);
+	}
+	@DeleteMapping("/delete/{id}")
+	public void del(@PathVariable int id){
+		productoService.delete(id);
+	}
+	@PutMapping("/update/{id}")
+	public Producto del(@RequestBody Producto prod, @PathVariable int id){
+		Producto p = productoService.read(id);
+		p.setNombre(prod.getNombre());
+		p.setPrecio(prod.getPrecio());
+		p.setCantidad(prod.getCantidad());
+		
+		return productoService.update(p);
 	}
 
 }
